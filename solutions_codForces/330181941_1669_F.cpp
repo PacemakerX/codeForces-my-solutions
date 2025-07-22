416aa8a6adc5b96b724329b33977ab040e8e2290
Sparsh <sparsh.officialwork@gmail.com>
2025-07-22 20:56:09 +0530

    adds 6 questions

---
+++ solutions_codForces/330181941_1669_F.cpp
@@ -0,0 +1,140 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type definitions
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+// Macros
+#define nline "\n"
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define forr(i, a, b) for (int i = a; i <= b; i++)
+#define ford(i, a, b) for (int i = a; i >= b; i--)
+#define elasped_time 1.0 * clock() / CLOCKS_PER_SEC
+
+// Constants
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+const double EPS = 1e-9;
+const double PI = acos(-1);
+
+ll mod_add(ll a, ll b, ll m = MOD) { return ((a % m) + (b % m)) % m; }
+ll mod_sub(ll a, ll b, ll m = MOD) { return ((a % m) - (b % m) + m) % m; }
+ll mod_mul(ll a, ll b, ll m = MOD) { return ((a % m) * (b % m)) % m; }
+
+ll mod_pow(ll base, ll exp, ll m = MOD)
+{
+    ll res = 1;
+    base %= m;
+    while (exp > 0)
+    {
+        if (exp & 1)
+            res = mod_mul(res, base, m);
+        base = mod_mul(base, base, m);
+        exp >>= 1;
+    }
+    return res;
+}
+
+ll mod_inv(ll a, ll m = MOD)
+{
+    return mod_pow(a, m - 2, m); // Only works if m is prime
+}
+
+ll mod_div(ll a, ll b, ll m = MOD)
+{
+    return mod_mul(a, mod_inv(b, m), m);
+}
+
+// Binary exponentiation (for non-modular calculations)
+ll binpow(ll base, ll exp)
+{
+    ll res = 1;
+    while (exp > 0)
+    {
+        if (exp & 1)
+            res *= base;
+        base *= base;
+        exp >>= 1;
+    }
+    return res;
+}
+
+/************/
+void solve()
+{
+    ll n;
+    cin >> n;
+
+    vl pre(n, 0LL), suff(n, 0LL);
+
+    forn(i, n)
+    {
+        cin >> pre[i];
+        suff[i] = pre[i];
+
+        if (i)
+            pre[i] += pre[i - 1];
+    }
+
+    ford(i, n - 2, 0)
+    {
+        suff[i] += suff[i + 1];
+    }
+
+    ll cnt = 0;
+
+    ll low = 0, high = n - 1;
+
+    while (low <high)
+    {
+        if (pre[low] < suff[high])
+            low++;
+        else if (pre[low] > suff[high])
+            high--;
+        else
+        {
+            cnt = (low + 1) + (n - high );
+            low++;
+            high--;
+        }
+    }
+
+    cout << cnt << nline;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("./outputs/input.txt", "r", stdin);
+    freopen("./outputs/output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
+// Explanatie
+
+/* */
