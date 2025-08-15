2ee003ee1b59b7f5455dccadea2cc6313da45c0d
Sparsh <sparsh.officialwork@gmail.com>
2025-08-16 00:34:23 +0530

    Adds 3 question rated >=1200, question for round 1874 and 1875 are same

---
+++ solutions_codForces/334035514_821_B.cpp
@@ -0,0 +1,127 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type definitions
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+// Macros
+#define nline "\n"
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define forr(i, a, b) for (int i = a; i <= b; i++)
+#define ford(i, a, b) for (int i = a; i >= b; i--)
+#define elasped_time 1.0 * clock() / CLOCKS_PER_SEC
+
+// clang-format off
+// Debug macros
+#define debarr(a,n) cout<<#a<<" : ";for(int i=0;i<n;i++) cerr<<a[i]<<" "; cerr<<endl;
+#define debmat(mat,row,col) cout<<#mat<<" :\n";for(int i=0;i<row;i++) {for(int j=0;j<col;j++) cerr<<mat[i][j]<<" ";cerr<<endl;}
+#define pr(...) dbs(#__VA_ARGS__, __VA_ARGS__)
+
+// Debug template functions
+template <class S, class T>ostream& operator <<(ostream& os, const pair<S, T>& p) {return os << "(" << p.first << ", " << p.second << ")";}
+template <class T>ostream& operator <<(ostream& os, const vector<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const unordered_set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const unordered_map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const multiset<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T> void dbs(string str, T t) {cerr << str << " : " << t << "\n";}
+template <class T, class... S> void dbs(string str, T t, S... s) {int idx = str.find(','); cerr << str.substr(0, idx) << " : " << t << ","; dbs(str.substr(idx + 1), s...);}
+template <class T> void prc(T a, T b) {cerr << "["; for (T i = a; i != b; ++i) {if (i != a) cerr << ", "; cerr << *i;} cerr << "]\n";}
+// clang-format on
+
+// Constants
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+const double EPS = 1e-9;
+const double PI = acos(-1);
+
+// Utility functions
+ll mod_add(ll a, ll b, ll m = MOD) { return ((a % m) + (b % m)) % m; }
+ll mod_sub(ll a, ll b, ll m = MOD) { return ((a % m) - (b % m) + m) % m; }
+ll mod_mul(ll a, ll b, ll m = MOD) { return ((a % m) * (b % m)) % m; }
+ll mod_pow(ll base, ll exp, ll m = MOD)
+{
+    ll res = 1;
+    base %= m;
+    while (exp > 0)
+    {
+        if (exp & 1)
+            res = mod_mul(res, base, m);
+        base = mod_mul(base, base, m);
+        exp >>= 1;
+    }
+    return res;
+}
+ll mod_inv(ll a, ll m = MOD)
+{
+    return mod_pow(a, m - 2, m); // Only works if m is prime
+}
+ll mod_div(ll a, ll b, ll m = MOD)
+{
+    return mod_mul(a, mod_inv(b, m), m);
+}
+
+ll binpow(ll base, ll exp, ll mod = MOD)
+{
+    ll res = 1;
+    base %= mod;
+    while (exp > 0)
+    {
+        if (exp & 1)
+            res = (res * base) % mod;
+        base = (base * base) % mod;
+        exp >>= 1;
+    }
+    return res;
+}
+/************/
+void solve()
+{
+    ll m,b;
+    cin>>m>>b;
+
+    ll res=0;
+    for(ll y=0;y<=b;y++){
+
+        ll x= m*(b-y);
+
+        ll ans=(((x+1)*(y+1)*(x+y))>>1);
+
+        res=max(res,ans);
+    }
+    cout<<res<<nline;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("./outputs/input.txt", "r", stdin);
+    freopen("./outputs/output.txt", "w", stdout);
+    cerr.rdbuf(cout.rdbuf());
+#endif
+
+    // int t;
+    // cin >> t;
+    // while (t--)
+     solve();
+
+    return 0;
+}
